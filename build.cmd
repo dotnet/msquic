4f27822780ad72486d32ec1d541fed9ff306ec03
Juan Hoyos <19413848+hoyosjs@users.noreply.github.com>
2023-03-29 09:25:30 -0700

    Allow for per-leg packaging and add osx-x64 package (#138)
    * Fence publishing in separate stage
    
    * Fix yml and use pool provider subs
    
    * Fix parameter lowering
    
    * Allow for per-leg packaging and add osx-x64 package
    
    Also add win-x64 leg generation of metapackage
    
    * Fix path casing
    
    * Fix os folder name

--- build.cmd
+++ build.cmd
@@ -2,5 +2,5 @@
 setlocal
 
 set PATH=%CD%\src\msquic\artifacts\dotnet-tools;%PATH%
-powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\common\Build.ps1""" -warnAsError:$false -restore -pack %*"
+powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\common\Build.ps1""" -warnAsError:$false -build -restore -pack %*"
 exit /b %ErrorLevel%
